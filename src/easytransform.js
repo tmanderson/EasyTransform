ccbeab82adeb551fc18fe82ac44dc88f4b3519b7
Mitch Anderson <thomasmitchellanderson@gmail.com>
2011-12-12 16:42:51 -0600

    Initial commit

---
+++ src/easytransform.js
@@ -0,0 +1,74 @@
+(function() {
+	function easyTransform(el){
+		this.el = el;
+	}
+
+	easyTransform.prototype = {
+		//binds a shortcut on this el to get the transform style.
+		bind: function() {
+			//shortcuts to computed style	
+		},
+
+		unbind: function() {
+			
+		},
+
+		stopTransform: function() {
+			
+		},
+
+		setRotation: function(a) {
+			
+		},
+
+		setTranslation: function(x, y) {
+
+		},
+
+		setScale: function(s) {
+			
+		},
+
+		setSkew: function(x, y) {
+
+		},
+
+		setTransform: function(t) {
+			
+		},
+
+		getFinishRotation: function() {
+			
+		},
+
+		getFinishTranslation: function() {
+			
+		},
+
+		getFinishScale: function() {
+			
+		},
+
+		getFinishTransform: function() {
+			
+		},
+
+		getLiveRotation: function() {
+			
+		},
+
+		getLiveTranslation: function() {
+			
+		},
+
+		getLiveScale: function() {
+			
+		},
+
+		getLiveTransform: function() {
+			
+		}
+	}
+
+	window.ezt = easyTransform;
+})();
